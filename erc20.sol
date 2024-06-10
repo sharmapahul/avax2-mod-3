fab485441cdde25fa476d232cf7d90500974ddd8
sharmapahul <165384030+sharmapahul@users.noreply.github.com>
2024-06-10 14:45:40 +0530

    Update erc20.sol

--- erc20.sol
+++ erc20.sol
@@ -1,1 +1,26 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, ERC20Burnable, Ownable {
+  constructor(address payable initialOwner)
+        ERC20("MALE", "MLE")
+        Ownable(initialOwner)
+    {}
+    function mint(uint256 amount) public onlyOwner {
+        _mint(msg.sender, amount);
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function anyoneBurn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+}
